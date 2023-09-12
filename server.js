aa880e45dd473f424668110cf53d04dca2780678
Vandilson <vandilson25@outlook.com>
2023-09-12 11:45:16 -0300

    changing server routes names

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 5502;
 const apiKey = "AIzaSyDxoDC-gcdR3js4c9hye0SijsYG6YukZX8";
 const baseApiUrl = 'https://www.googleapis.com/youtube/v3';
 
-// Videos Searched By Query
+
+// Most Popular Videos
 //https://www.googleapis.com/youtube/v3/search?key=apiKey&type=video&part=snippet&chart=mostPopular
-app.get('/search', async (req, res, next) => {
+app.get('/search/mostPopular', async (req, res, next) => {
   try {
     
     res.setHeader('Permissions-Policy', 'feature=value');
     
-    const searchQuery = req.query.search_mostPopular;
+    //const searchQuery = req.query.search_mostPopular;
     const url = `${baseApiUrl}/search?key=${apiKey}&type=video&part=snippet&chart=mostPopular`
     const response = await axios.get(url);
-    
+    console.log(response)
   }
   catch(error) {
-    next(error)
+    console.log(error)
   }
     
 })
 
 
-// Most Popular Videos
+// Videos Searched By Query
 // https://www.googleapis.com/youtube/v3/search?key=apiKey&type=video&part=snippet&q=palmeiras
-app.get('/search', async (req, res, next) => {
+app.get('/search/query', async (req, res, next) => {
   try {
     
     res.setHeader('Permissions-Policy', 'feature=value');
@@ -37,7 +38,7 @@ app.get('/search', async (req, res, next) => {
     console.log(response)
   }
   catch(error) {
-    next(error)
+    console.log(error)
   }
     
 })
