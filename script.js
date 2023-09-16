6ae223afa451314cfc8598a64303162891b870d7
Vandilson <vandilson25@outlook.com>
2023-09-16 12:02:54 -0300

    adding channel title and video published time

--- script.js
+++ script.js
@@ -17,8 +17,55 @@
         .then(response => {
     
             const videoIds = response.data.items.map((items) => items.id.videoId);
-
             const channelsId = response.data.items.map((item) => item.snippet.channelId);
+            const channelTitle = response.data.items.map((item) => item.snippet.channelTitle)
+            console.log(channelTitle)
+
+            const videosPublishedTime = response.data.items.map((item) => item.snippet.publishedAt)
+            console.log(videosPublishedTime)
+
+            let formattedDate = [];
+
+            videosPublishedTime.forEach((video) => {
+              
+                function formatarTempo(diferenca) {
+
+                    const segundosPorMinuto = 60;
+                    const minutosPorHora = 60;
+                    const horasPorDia = 24;
+                    const diasPorAno = 365.25; 
+                    const diasPorMes = 30.44; 
+                    const mesesPorAno = 12;
+                  
+                    if (diferenca < segundosPorMinuto) {
+                      return `Publicado há ${Math.floor(diferenca)} segundos`;
+                    } 
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / segundosPorMinuto)} minutos`);
+                    } 
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora)) === 1? 'hora' : 'horas'}`);
+                    } 
+                      
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes * mesesPorAno)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes)) === 1 ? 'mês' : 'meses'}`);
+                    }
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia)) === 1 ? 'dia' : 'dias'}`);
+                    }
+                    else {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno)) === 1 ? 'ano' : 'anos'}`);
+                    }
+                }
+
+                const currentDate = new Date();
+                const dataPublicacao = new Date(video);
+                const diferencaEmSegundos = (currentDate - dataPublicacao) / 1000; // Convertendo para segundos
+                formatarTempo(diferencaEmSegundos);
+              
+                console.log(formattedDate);
+                  
+            })
             
 
             videoIds.forEach((videoId, index) => {
@@ -29,29 +76,37 @@
                 const titles = response.data.items.map((item) => item.snippet.title)
 
 
-                console.log(channelsId[index])
+                /* console.log(channelsId[index]) */
                 axios.get(`${baseApiUrl}/channels?part=snippet&id=${channelsId[index]}&key=${apiKey}`)
                 .then(response => {
                 const channelThumbnail = response.data.items.map((item) => item.snippet.thumbnails.default.url);
                 
-
+                
                 videoContainer.innerHTML += `
 
-                        <div class="w-[320px] h-fit bg-white mb-10">
-                        <div class="w-[320px] h-[180px] bg-slate-400">
-                            <iframe src="${videoPlayerSrc}"></iframe>
+                    <div class="w-[300px] h-fit mb-10">
+                        <div class="w-[300px] h-[155px]">
+                            <iframe class="" src="${videoPlayerSrc}"></iframe>
                         </div>
-                        <div class="w-full h-full px-3 pt-2 ">
-                            <div class="flex gap-3">
-                                <div class="w-10 h-10 rounded-full "><img class="w-full h-full rounded-full" src="${channelThumbnail}" alt=""></div>
+                        <div class="w-full h-full pt-2 ">
+                        <div class="w-full flex gap-3 " style="display: grid; grid-template-areas: 'image-container info-video-container'; grid-template-rows: 1; grid-template-columns: auto 2fr;">
+                                <div class="rounded-full">
+                                    <img class="w-10 h-10 rounded-full" src="${channelThumbnail}" alt="">
+                                </div>
                                 <div class="">
-                                    <div class=""><p class="text-[#0f0f0fcb] text-lg font-bold">${titles[videoIds.indexOf(videoId)]}</p></div>
-                                    <div class="mt-1  "><p class="text-sm text-[#606060]">Nome do canal</p></div>
-                                    <div class=" "><p class="text-sm text-[#606060]">Postado há 50 min</p></div>
+                                <div class="w-full max-h-[45px] overflow-hidden custom-text">
+                                    <p class="text-[#0f0f0fcb] text-base font-bold">${titles[videoIds.indexOf(videoId)]}</p>
+                                    </div>
+                                    <div class="mt-2">
+                                      <p class="text-sm text-[#606060]">${channelTitle[index]}</p>
+                                    </div>
+                                    <div class=" ">
+                                      <p class="text-sm text-[#606060]">${formattedDate[index]}</p>
+                                    </div>
                                 </div>
                                 </div>
                             </div>
-                        </div>
+                    </div>
                 
                 `
                 })
@@ -64,7 +119,7 @@
         });
 
     }
-    searchVideosMostPopular();
+    /* searchVideosMostPopular(); */
     
 
     function searchVideosByQuery() {
@@ -81,7 +136,8 @@
         // Fazer uma solicitação ao seu backend para buscar vídeos
         axios.get(`${baseApiUrl}/search?key=${apiKey}&type=video&part=snippet&q=${searchQuery}`)
         .then(response => {
-            const videoIds = response.data.items.map((item) => item.id.videoId)
+            
+          /* const videoIds = response.data.items.map((item) => item.id.videoId)
             const titles = response.data.items.map((item) => item.snippet.title)
             console.log(videoIds)
             console.log(titles)
@@ -106,7 +162,109 @@
             videoDiv.appendChild(titleWrapper);
             videoContainer.appendChild(videoDiv);
             
-            });
+            }); */
+
+
+            const videoIds = response.data.items.map((items) => items.id.videoId);
+            const channelsId = response.data.items.map((item) => item.snippet.channelId);
+            const channelTitle = response.data.items.map((item) => item.snippet.channelTitle)
+            console.log(channelTitle)
+
+            const videosPublishedTime = response.data.items.map((item) => item.snippet.publishedAt)
+            console.log(videosPublishedTime)
+
+            let formattedDate = [];
+
+            videosPublishedTime.forEach((video) => {
+              
+                function formatarTempo(diferenca) {
+
+                    const segundosPorMinuto = 60;
+                    const minutosPorHora = 60;
+                    const horasPorDia = 24;
+                    const diasPorAno = 365.25; 
+                    const diasPorMes = 30.44; 
+                    const mesesPorAno = 12;
+                  
+                    if (diferenca < segundosPorMinuto) {
+                      return `Publicado há ${Math.floor(diferenca)} segundos`;
+                    } 
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / segundosPorMinuto)} minutos`);
+                    } 
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora)) === 1? 'hora' : 'horas'}`);
+                    } 
+                      
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes * mesesPorAno)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes)) === 1 ? 'mês' : 'meses'}`);
+                    }
+                    else if (diferenca < (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno)) {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia)) === 1 ? 'dia' : 'dias'}`);
+                    }
+                    else {
+                      return formattedDate.push(`Publicado há ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno))} ${Math.floor(diferenca / (segundosPorMinuto * minutosPorHora * horasPorDia * diasPorAno)) === 1 ? 'ano' : 'anos'}`);
+                    }
+                }
+
+                const currentDate = new Date();
+                const dataPublicacao = new Date(video);
+                const diferencaEmSegundos = (currentDate - dataPublicacao) / 1000; // Convertendo para segundos
+                formatarTempo(diferencaEmSegundos);
+              
+                console.log(formattedDate);
+                  
+            })
+            
+
+            videoIds.forEach((videoId, index) => {
+
+                const videoPlayerSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&showinfo=0&controls=0&modestbranding=1&allowfullscreen`;
+
+                
+                const titles = response.data.items.map((item) => item.snippet.title)
+
+
+                /* console.log(channelsId[index]) */
+                axios.get(`${baseApiUrl}/channels?part=snippet&id=${channelsId[index]}&key=${apiKey}`)
+                .then(response => {
+                const channelThumbnail = response.data.items.map((item) => item.snippet.thumbnails.default.url);
+                
+                
+                videoContainer.innerHTML += `
+
+                    <div class="w-[300px] h-fit mb-10">
+                        <div class="w-[300px] h-[155px]">
+                            <iframe class="" src="${videoPlayerSrc}"></iframe>
+                        </div>
+                        <div class="w-full h-full pt-2 ">
+                        <div class="w-full flex gap-3 " style="display: grid; grid-template-areas: 'image-container info-video-container'; grid-template-rows: 1; grid-template-columns: auto 2fr;">
+                                <div class="rounded-full">
+                                    <img class="w-10 h-10 rounded-full" src="${channelThumbnail}" alt="">
+                                </div>
+                                <div class="">
+                                <div class="w-full max-h-[45px] overflow-hidden custom-text">
+                                    <p class="text-[#0f0f0fcb] text-base font-bold">${titles[videoIds.indexOf(videoId)]}</p>
+                                    </div>
+                                    <div class="mt-2">
+                                      <p class="text-sm text-[#606060]">${channelTitle[index]}</p>
+                                    </div>
+                                    <div class=" ">
+                                      <p class="text-sm text-[#606060]">${formattedDate[index]}</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                    </div>
+                
+                `
+                })
+          
+        });
+
+
+
+
         })
         .catch(error => {
             console.error(error);
